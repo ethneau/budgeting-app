4a8d7db49dfadbd7048fc810440759a56c13eaea
Ethne Au <eth.neau515@gmail.com>
2024-01-11 20:46:19 -0500

    edited main layout and balance

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let bal = 1.0;
 function changeColor(element, color) {
   element.style.backgroundColor = color;
 }
+function updateElement() {
+  var newValue = bal;
+  newValue = bal.toFixed(2);
+  document.getElementById("bal").innerHTML = "$" + newValue;
+}
+
+// Call the function when the page loads
+window.onload = function () {
+  updateElement();
+};
