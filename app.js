75dd3266a4b169655d72ac460ab44bac1ead8102
Ethne Au <eth.neau515@gmail.com>
2024-01-17 17:36:15 -0500

    modified data screen

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-let bal = 0.0;
+if (!localStorage.getItem("bal")) localStorage.setItem("bal", 10.0);
+bal = parseFloat(localStorage.getItem("bal"));
+
 function changeColor(element, color) {
   element.style.backgroundColor = color;
 }
 function updateElement() {
-  var newValue = bal;
-  newValue = bal.toFixed(2);
-  document.getElementById("bal").innerHTML = "$" + newValue;
+  if (isNaN(bal.toFixed(2)))
+    document.getElementById("bal").innerHTML = "$" + 0.0;
+  else document.getElementById("bal").innerHTML = "$" + bal.toFixed(2);
 }
-
-// Call the function when the page loads
-window.onload = function () {
-  updateElement();
-};
+function saveBalance() {
+  let newBal = document.getElementById("newbal").value;
+  let newValue = parseFloat(newBal);
+  if (isNaN(newValue)) document.getElementById("update").innerHTML = "$" + bal;
+  else {
+    document.getElementById("update").innerHTML = "$" + newValue.toFixed(2);
+    localStorage.setItem("bal", newValue);
+    bal = localStorage.getItem("bal");
+  }
+}
+function setNewBalance() {
+  if (isNaN(bal)) document.getElementById("newbal").value = 0.0;
+  else {
+    document.getElementById("newbal").value = bal.toFixed(2);
+  }
+}
